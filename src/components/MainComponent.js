801fdfb18f11d7b1b0d67ea7461d6801d7f551d4
sulesuleman <mousuleman@gmail.com>
2020-06-14 00:27:17 +0500

    Controlled Forms

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -7,6 +7,7 @@ import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import Home from './HomeComponent';
 import Contact from './ContactComponent';
+import About from './AboutComponent';
 import { COMMENTS } from '../shared/comments';
 import { PROMOTIONS } from '../shared/promotions';
 import { LEADERS } from '../shared/leaders';
@@ -50,6 +51,7 @@ class Main extends Component {
               <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
               <Route exact path='/contactus' component={Contact} />} />
               <Route path='/menu/:dishId' component={DishWithId} />
+              <Route exact path='/aboutus' component={()=><About leaders={this.state.leaders}/>} />
               <Redirect to="/home" />
           </Switch>
         <Footer />
